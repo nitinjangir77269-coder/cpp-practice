9b769cf8db94cf320b21dd74e075edb76bca5c4a
nitinjangir77269-coder <nitinjangir77269@gmail.com>
2026-06-10 18:57:43 +0530

    Create q3.cpp

---
+++ q3.cpp
@@ -0,0 +1,16 @@
+// Q3 = to print even number from 1-->N
+
+#include<iostream>
+using namespace std ;
+int main()
+{
+    int i , num ;
+    cout<<"enetr numebr   = ";
+    cin>>num ;
+    for(i=2; i<=num; i=i+2)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+
+}
