3686a92e8b0ad3013b0ebe82d5448839241e3f3e
nitinjangir77269-coder <nitinjangir77269@gmail.com>
2026-06-10 18:59:03 +0530

    Create q4.cpp

---
+++ q4.cpp
@@ -0,0 +1,14 @@
+// Q4 = even number from n-->0
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    int i , num ;
+    cout<<"enter number  =";
+    cin>>num;
+    for(i =num ; i>=2; i= i-2)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
