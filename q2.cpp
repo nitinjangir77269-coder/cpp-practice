07954ce34923ad457c4764c9ff60db41917e370b
nitinjangir77269-coder <nitinjangir77269@gmail.com>
2026-06-10 18:55:07 +0530

    Create q2.cpp

---
+++ q2.cpp
@@ -0,0 +1,14 @@
+// Q2 = to print number N-->1
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    int num , i ;
+    cout<<"enter the number  =";
+    cin>>num;
+    for(i = num; i>0; i-- )
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
