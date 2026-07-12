3c1235eb5734423c2fd1aee0f765d5d2c6734a51
nitinjangir77269-coder <nitinjangir77269@gmail.com>
2026-07-12 13:38:45 +0530

    Create 4fun.cpp

---
+++ 4fun.cpp
@@ -0,0 +1,68 @@
+// #include<iostream>
+// using namespace std;
+// int add(int a, int b)
+// {
+//     int sum;
+//     sum = a+b;
+//     return sum;
+// }
+// int main()
+// {
+//     int x,y, sum;
+//     cout<<"entert x and y";
+//     cin>>x>>y;
+//     sum = add(x,y);
+//     cout<<"sum ="<<sum;
+//     return 0;
+    
+// }
+// #include<iostream>
+// using namespace std;
+// int add()
+// {
+//     int a, b, sum ;
+//     cout<<"enter a and b";
+//     cin>>a>>b;
+//     sum = a+b;
+//     return sum ;
+
+// }
+// int main ()
+// {
+//     int sum;
+//     sum = add();
+//     cout<<sum;
+//     return 0;
+// }
+// #include<iostream>
+// using namespace  std;
+// void add()
+// {
+//     int a, b, sum;
+//     cout<<"x  and y";
+//     cin>>a>>b;
+//     sum = a+b;
+//     cout<<sum ;
+//     }
+// int main ()
+// {
+//    add();
+//    return 0;
+// }
+
+// #include<iostream>
+// using namespace std ;
+// void add(int a , int b)
+// {
+//     int sum =a+b;
+//     cout<<sum;
+
+// }
+// int main()
+// {
+//     int x, y , sum ;
+//     cin>>x>>y;
+//     add(x ,y);
+    
+//     return 0;
+// }
